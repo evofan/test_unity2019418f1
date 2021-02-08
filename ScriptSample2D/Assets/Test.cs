af4c1a6a07b2e900ae1b5b4b6dbed0430288bd85
evofan <evofan@outlook.jp>
2021-02-08 22:14:24 +0900

    add sample script

---
+++ ScriptSample2D/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() // 最初に一回だけ実行
+    {
+        // ここにクラスの処理を書く
+
+        // Hello, Worldを表示する
+        Debug.Log("Hello, World.");
+        
+        // p65
+        // 変数
+        // ■int, 整数型, -2,147,483,648 ～ 2,147,483,647
+        // ■
+
+    }
+
+    // Update is called once per frame
+    void Update() // エンターフレームと同じ、60fpsだが、実際はTime.deltaTime毎に実行
+    {
+        
+    }
+}
