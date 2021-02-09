a6188bbd5483ed01ee867bafbefecf29706b58fb
evofan <evofan@outlook.jp>
2021-02-10 07:54:38 +0900

    fix

--- ScriptSample2D/Assets/Test.cs
+++ ScriptSample2D/Assets/Test.cs
@@ -11,11 +11,36 @@ public class Test : MonoBehaviour
 
         // Hello, Worldを表示する
         Debug.Log("Hello, World.");
-        
+
         // p65
         // 変数
         // ■int, 整数型, -2,147,483,648 ～ 2,147,483,647
-        // ■
+        // ■float, 浮動小数点型, -3.402823E+38 ～ 3.402823E+38
+        // ■double, 倍精度浮動小数点型, -1.79769313486232E+308 ～ 1.79769313486232E+308
+        // ■bool, ブール型, true or false
+        // ■char, 文字型, ★テキストで使用されるUnicode記号
+        // ■string, 文字列型, テキスト
+
+        int age; // 型名 変数名
+        age = 30;
+        Debug.Log(age);
+
+        // p66 変数の初期化と代入
+        float hight1 = 160.5f; // fを付けてfloat指定。つけないとdouble指定になるので注意
+        float hight2;
+        hight2 = hight1;
+        Debug.Log(hight2);
+
+        // 文字列
+        string name;
+        name = "Kitamura";
+        Debug.Log(name);
+
+        // 四則演算
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+
 
     }
 
